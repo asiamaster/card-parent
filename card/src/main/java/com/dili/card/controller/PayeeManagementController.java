b658610a8e7da63d96f423795711527060f7f895
zhangxing <zhangxing@diligrp.com>
2020-06-18 14:24:36 +0800

    领款交款管理api

---
+++ card/src/main/java/com/dili/card/controller/PayeeManagementController.java
@@ -0,0 +1,13 @@
+package com.dili.card.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 账务管理
+ */
+@RestController
+@RequestMapping(value = "/financial")
+public class PayeeManagementController {
+	
+}
--- card/src/main/java/com/dili/card/controller/PayerManagementController.java
+++ card/src/main/java/com/dili/card/controller/PayerManagementController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping(value = "/financial")
-public class FinancialManagementController {
+public class PayerManagementController {
 	
 }
