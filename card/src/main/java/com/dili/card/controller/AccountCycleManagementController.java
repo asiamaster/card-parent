abb0c20cb6ff9294bf6671101ed27b07c229aec7
zhangxing <zhangxing@diligrp.com>
2020-06-18 14:34:16 +0800

    账务周期

--- card/src/main/java/com/dili/card/controller/AccountCycleManagementController.java
+++ card/src/main/java/com/dili/card/controller/AccountCycleManagementController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping(value = "/financial")
-public class PayerManagementController {
+public class AccountCycleManagementController {
 	
 }
