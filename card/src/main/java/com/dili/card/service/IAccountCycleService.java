75c759e130a8ddab51df9ca2573938a98c8a8f1a
zhangxing <zhangxing@diligrp.com>
2020-06-22 15:55:50 +0800

    账务周期相关信息

--- card/src/main/java/com/dili/card/service/IAccountCycleService.java
+++ card/src/main/java/com/dili/card/service/IAccountCycleService.java
@@ -1,5 +1,30 @@
 package com.dili.card.service;
 
+import java.util.List;
+
+import com.dili.card.dto.AccountCycleDto;
+
+/**
+ * 账务周期服务
+ * @author zhangxing
+ */
 public interface IAccountCycleService {
+
+	/**
+	 * 对账
+	 */
+	void checkAccount(Long id);
+
+	/**
+	 * 账务周期列表
+	 */
+	List<AccountCycleDto> list(AccountCycleDto accountCycleDto);
+
+	/**
+	 * 账务周期详情
+	 */
+	AccountCycleDto detail(Long id);
+	
+	
 
 }
