896b23ac3a996430c0d81142bcec21c838780145
zhangxing <zhangxing@diligrp.com>
2020-06-22 14:08:29 +0800

    接口统一

--- card/src/main/java/com/dili/card/controller/ContractManagementController.java
+++ card/src/main/java/com/dili/card/controller/ContractManagementController.java
@@ -1,12 +1,16 @@
 package com.dili.card.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dili.card.dto.FundContractQueryDto;
 import com.dili.card.dto.FundContractRequestDto;
+import com.dili.card.dto.FundContractResponseDto;
 import com.dili.card.service.IContractService;
 import com.dili.ss.domain.BaseOutput;
 
@@ -32,18 +36,16 @@ public class ContractManagementController {
 	 * 列表合同
 	 */
 	@PostMapping("/list")
-	public BaseOutput<Boolean> list(@RequestBody FundContractRequestDto fundContractRequest) {
-		iContractService.list(fundContractRequest);
-		return BaseOutput.success();
+	public BaseOutput<List<FundContractResponseDto>> list(@RequestBody FundContractQueryDto contractQueryDto) {
+		return BaseOutput.successData(iContractService.list(contractQueryDto));
 	}
 	
 	/**
 	 * 详情合同
 	 */
 	@PostMapping("/detail")
-	public BaseOutput<Boolean> detail(@RequestBody FundContractRequestDto fundContractRequest) {
-		iContractService.detail(fundContractRequest);
-		return BaseOutput.success();
+	public BaseOutput<FundContractResponseDto> detail(@RequestBody FundContractRequestDto fundContractRequest) {
+		return BaseOutput.successData(iContractService.detail(fundContractRequest));
 	}
 	
 	/**
