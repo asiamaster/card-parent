d6f1c79eb10e3a078a8a44e2c2a79b352f0a7ce3
miaoguoxin <398544597@qq.com>
2020-06-29 09:34:24 +0800

    页面异常处理

--- card/src/main/java/com/dili/card/exception/global/GenericGlobalExceptionResolver.java
+++ card/src/main/java/com/dili/card/exception/global/GenericGlobalExceptionResolver.java
@@ -1,11 +1,14 @@
 package com.dili.card.exception.global;
 
+import com.alibaba.fastjson.JSON;
 import com.dili.card.exception.CardAppBizException;
 import com.dili.ss.constant.ResultCode;
 import com.dili.ss.domain.BaseOutput;
 import com.dili.ss.exception.BusinessException;
+import com.dili.ss.util.SpringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
@@ -13,18 +16,28 @@ import org.springframework.validation.ObjectError;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.ConstraintViolationException;
+import java.io.IOException;
 import java.util.List;
 
 /**
  * @author: miaoguoxin
  * @date: 2020/4/8 11:22
  */
-@RestControllerAdvice
+@ControllerAdvice
+@RequestMapping("/error")
 public class GenericGlobalExceptionResolver {
+    @Autowired
+    private HttpServletRequest request;
+    @Autowired
+    private HttpServletResponse response;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GenericGlobalExceptionResolver.class);
 
@@ -33,76 +46,84 @@ public class GenericGlobalExceptionResolver {
      * 方法参数校验处理，配合validator
      */
     @ExceptionHandler({MethodArgumentNotValidException.class})
-    public BaseOutput<?> handleMethodArgException(MethodArgumentNotValidException ex) {
-
-        return BaseOutput.create(ResultCode.PARAMS_ERROR,
-                errorMessage(ex.getBindingResult()));
+    public String handleMethodArgException(MethodArgumentNotValidException e) throws IOException {
+        return this.writeErrorResp(ResultCode.PARAMS_ERROR, errorMessage(e.getBindingResult()), e);
     }
 
     /**
      * 参数断言异常处理
      */
     @ExceptionHandler({IllegalArgumentException.class})
-    public BaseOutput<?> handleIllegalArgsException(IllegalArgumentException ex) {
-        return BaseOutput.create(ResultCode.PARAMS_ERROR, ex.getMessage());
+    public String handleIllegalArgsException(IllegalArgumentException e) throws IOException {
+        return this.writeErrorResp(ResultCode.PARAMS_ERROR, e.getMessage(), e);
     }
 
     /**
      * 单参数校验异常处理
      */
     @ExceptionHandler({ConstraintViolationException.class})
-    public BaseOutput<?> handleConstraintViolationException(ConstraintViolationException ex) {
-        return BaseOutput.create(ResultCode.PARAMS_ERROR, ex.getMessage());
+    public String handleConstraintViolationException(ConstraintViolationException e) throws IOException {
+        return this.writeErrorResp(ResultCode.PARAMS_ERROR, e.getMessage(), e);
     }
 
     /**
      * 415处理
      */
     @ExceptionHandler({HttpMediaTypeNotSupportedException.class})
-    public BaseOutput<?> handle415Exception(HttpMediaTypeNotSupportedException ex) {
-        return BaseOutput.create(ResultCode.UNSUPPORTED_MEDIA_TYPE, "Unsupported Media Type");
+    public String handle415Exception(HttpMediaTypeNotSupportedException e) throws IOException {
+        return this.writeErrorResp(ResultCode.UNSUPPORTED_MEDIA_TYPE, "Unsupported Media Type", e);
     }
 
     /**
      * 400处理
      */
     @ExceptionHandler({HttpMessageNotReadableException.class})
-    public BaseOutput<?> handle400Exception(HttpMessageNotReadableException ex) {
-        LOGGER.error("参数有误：{}", ex.getMessage());
-        return BaseOutput.create(ResultCode.INVALID_REQUEST, "Bad Request");
+    public String handle400Exception(HttpMessageNotReadableException e) throws IOException {
+        LOGGER.error("参数有误：{}", e.getMessage());
+        return this.writeErrorResp(ResultCode.INVALID_REQUEST, "Bad Request", e);
     }
 
     /**
      * 405处理
      */
     @ExceptionHandler({HttpRequestMethodNotSupportedException.class})
-    public BaseOutput<?> handle405Exception(HttpRequestMethodNotSupportedException ex) {
-        return BaseOutput.create(ResultCode.METHOD_NOT_ALLOWED, "Method Not Allowed");
+    public String handle405Exception(HttpRequestMethodNotSupportedException e) throws IOException {
+        return this.writeErrorResp(ResultCode.METHOD_NOT_ALLOWED, "Method Not Allowed", e);
     }
 
     /**
      * 业务异常处理
      */
     @ExceptionHandler({BusinessException.class})
-    public BaseOutput<?> handlerBusinessException(BusinessException e) {
-        return BaseOutput.create(e.getErrorCode(), e.getMessage());
+    public String handlerBusinessException(BusinessException e) throws IOException {
+        return this.writeErrorResp(e.getErrorCode(), e.getMessage(), e);
     }
 
     /**
      * 业务异常处理
      */
     @ExceptionHandler({CardAppBizException.class})
-    public BaseOutput<?> handlerAppException(CardAppBizException e) {
-        return BaseOutput.create(e.getCode(), e.getMessage());
+    public String handlerAppException(CardAppBizException e) throws IOException {
+        return this.writeErrorResp(e.getCode(), e.getMessage(), e);
     }
 
     /**
      * 处理未自定义的异常
      */
     @ExceptionHandler({Exception.class})
-    public BaseOutput<?> handlerOtherException(Exception e) {
+    public String handlerOtherException(Exception e) throws IOException {
         LOGGER.error("其他异常:{}", e);
-        return BaseOutput.failure("服务异常，请联系管理员");
+        return this.writeErrorResp(ResultCode.APP_ERROR, "服务异常，请联系管理员", e);
+    }
+
+    private String writeErrorResp(String errorCode, String errorMsg, Exception e) throws IOException {
+        if (request.getRequestURI().contains(".html")) {
+            request.setAttribute("exception", e);
+            return SpringUtil.getProperty("error.page.default", "error/default");
+        }
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().write(JSON.toJSONString(BaseOutput.create(errorCode, errorMsg)));
+        return null;
     }
 
     private static String errorMessage(BindingResult result) {
@@ -114,5 +135,10 @@ public class GenericGlobalExceptionResolver {
                 .findFirst()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .orElse("未知参数错误");
+    }
+
+    @RequestMapping(value = "handleError",produces = "text/html")
+    public String handleError(HttpServletRequest request, HttpServletResponse response){
+        return SpringUtil.getProperty("error.page.default", "error/default");
     }
 }
