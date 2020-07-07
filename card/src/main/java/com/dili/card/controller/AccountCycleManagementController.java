593675a1f8a8fcc57ab49ce8a5470b7ae7138b78
zhangxing <zhangxing@diligrp.com>
2020-07-07 11:03:17 +0800

    账务周期详情

--- card/src/main/java/com/dili/card/controller/AccountCycleManagementController.java
+++ card/src/main/java/com/dili/card/controller/AccountCycleManagementController.java
@@ -4,14 +4,20 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSON;
+import com.dili.card.common.serializer.EnumTextDisplayAfterFilter;
 import com.dili.card.dto.AccountCycleDto;
+import com.dili.card.exception.CardAppBizException;
 import com.dili.card.service.IAccountCycleService;
+import com.dili.ss.constant.ResultCode;
 import com.dili.ss.domain.BaseOutput;
 import com.dili.ss.domain.PageOutput;
 
@@ -38,8 +44,14 @@ public class AccountCycleManagementController {
     * 跳转详情
     * @date 2020/7/6
     */
-    @GetMapping("/detail.html")
-    public String detailFacadeView() {
+    @GetMapping("/detail.html/{id}")
+    public String detailFacadeView(@PathVariable Long id, ModelMap map) {
+    	if (id == null || id < 0L) {
+    		 throw new CardAppBizException(ResultCode.PARAMS_ERROR, "账务周期详情请求参数错误");
+		}
+    	String json = JSON.toJSONString(iAccountCycleService.detail(id),
+                new EnumTextDisplayAfterFilter());
+        map.put("detail", JSON.parseObject(json));   
         return "cycle/detail";
     }
 
