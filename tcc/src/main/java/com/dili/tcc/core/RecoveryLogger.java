02ad2330b8ebbc7b6c37ada3f16fe26d87936ea6
miaoguoxin <398544597@qq.com>
2020-07-16 11:23:17 +0800

    提交

--- tcc/src/main/java/com/dili/tcc/core/RecoveryLogger.java
+++
@@ -1,9 +0,0 @@
-package com.dili.tcc.core;
-
-/**
- * 日志记录与恢复
- * @Auther: miaoguoxin
- * @Date: 2020/7/15 16:58
- */
-public class RecoveryLogger {
-}
--- tcc/src/main/resources/info.md
+++
@@ -1,3 +0,0 @@
-提供一个统一的tcc模板调用方式<br>
-目前不支持hystrix<br>
-在rpc接口上添加@Tcc
