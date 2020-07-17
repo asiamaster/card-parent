d66f58a8f0c70d3cf2ed3f0b52a97d7056d34010
xuliang <514132096@qq.com>
2020-07-17 09:20:32 +0800

    根据资金项目梳理重新调整资金项目枚举；

--- card/src/main/java/com/dili/card/type/FundItem.java
+++ card/src/main/java/com/dili/card/type/FundItem.java
@@ -5,26 +5,31 @@ package com.dili.card.type;
  */
 public enum FundItem {
 
-    CASH_CHARGE(1, "现金充值"),
-    POS_CHARGE(2, "POS充值"),
-    EBANK_CHARGE(3, "网银充值"),
+    CASH_CHARGE(1, "现金存款"),
+    POS_CHARGE(2, "POS存款"),
+    EBANK_CHARGE(3, "网银存款"),
     CASH_WITHDRAW(4, "现金取款"),
     EBANK_WITHDRAW(5, "网银取款"),
     IC_CARD_COST(6, "IC工本费"),
     EBANK_SERVICE(7, "网银手续费"),
     POS_SERVICE(8, "POS手续费"),
-    MANDATORY_FREEZE(9, "手动冻结"),
-    MANDATORY_UNFREEZE(10, "手动解冻"),
-    TRADE_FREEZE(11, "交易冻结"),
-    TRADE_UNFREEZE(12, "交易解冻"),
-    CAR_ENTER(13, "车辆进场费"),
-    CAR_OUTER(14, "车辆出场费"),
-    WEIGH_SERVICE(15, "称重服务费"),
-    PASSPORT(16, "通行证费"),
-    TRANSFER(17, "转场费"),
-    LEAVE(18, "离场费"),
-    TRADE_SERVICE(19, "交易服务费"),
-    TRADE_PAYMENT(20, "交易货款");
+    TRADE_FREEZE(9, "交易冻结"),
+    TRADE_UNFREEZE(10, "交易解冻"),
+    TRADE_FREEZE_UNFREEZE(11, "交易冻结解冻"),
+    MANDATORY_FREEZE_FUND(12, "手动冻结资金"),
+    MANDATORY_UNFREEZE_FUND(13, "手动解冻资金"),
+    MANDATORY_FREEZE_ACCOUNT(14, "手动冻结账户"),
+    MANDATORY_UNFREEZE_ACCOUNT(15, "手动解冻账户"),
+    LOCAL_DELIVERY_FEE(16, "本地配送费"),
+    CAR_ENTRANCE_FEE(17, "车辆进场费"),
+    CAR_OUT_FEE(18, "车辆出场费"),
+    WEIGH_SERVICE_FEE(19, "称重服务费"),
+    TRANSFER_FEE(20, "转场费"),
+    LEAVE_FEE(21, "离场费"),
+    TRADE_PAYMENT(22, "货款"),
+    TRADE_SERVICE_FEE(23, "交易手续费"),
+    PASSPORT_MANAGE_FEE(24, "通行证管理费"),
+    RETURN_CARD_SERVICE_FEE(25, "退卡手续费");
     private int code;
     private String name;
 
