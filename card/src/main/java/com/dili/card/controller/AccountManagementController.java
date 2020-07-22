f4d0fca5bdee863a695384c240146651e0863fe3
wangbo <wangbo@diligrp.com>
2020-07-22 18:33:37 +0800

    增加账户冻结解冻代码

--- card/src/main/java/com/dili/card/controller/AccountManagementController.java
+++ card/src/main/java/com/dili/card/controller/AccountManagementController.java
@@ -1,6 +1,8 @@
 package com.dili.card.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -12,4 +14,39 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/account")
 public class AccountManagementController {
 
+	/**
+	 * 冻结账户页面
+	 */
+	@GetMapping("/frozenAccount.html")
+	public String frozenAccountView(Long accountId, ModelMap map) {
+		map.put("accountId", accountId);
+		return "accountquery/frozenAccount";
+	}
+	
+	/**
+	 * 解冻账户页面
+	 */
+	@GetMapping("/unfrozenAccount.html")
+	public String unfrozenAccountView(Long accountId, ModelMap map) {
+		map.put("accountId", accountId);
+		return "accountquery/unfrozenAccount";
+	}
+	
+	/**
+	 * 冻结账户
+	 */
+	@GetMapping("/frozenAccount.action")
+	public String frozenAccount(Long accountId, ModelMap map) {
+		map.put("accountId", accountId);
+		return "fund/unfrozenFund";
+	}
+	
+	/**
+	 * 解冻账户
+	 */
+	@GetMapping("/unfrozenAccount.action")
+	public String unfrozenAccount(Long accountId, ModelMap map) {
+		map.put("accountId", accountId);
+		return "fund/unfrozenFund";
+	}
 }
--- card/src/main/java/com/dili/card/service/IFundService.java
+++ card/src/main/java/com/dili/card/service/IFundService.java
@@ -1,7 +1,9 @@
 package com.dili.card.service;
 
 import com.dili.card.dto.FundRequestDto;
+import com.dili.card.dto.FundUnfrozenRecordDto;
 import com.dili.card.dto.UnfreezeFundDto;
+import com.dili.ss.domain.PageOutput;
 
 /**
  * 资金操作service接口
@@ -26,9 +28,14 @@ public interface IFundService {
 	void frozen(FundRequestDto fundRequestDto);
 
 	/**
-	 * 解冻资金
+	 * 未解冻资金记录
 	 * 
 	 * @param fundRequestDto
+	 */
+	PageOutput<FundUnfrozenRecordDto> unfrozenRecord(UnfreezeFundDto unfreezeFundDto);
+	
+	/**
+	 * 解冻资金,支持批量操作
 	 */
 	void unfrozen(UnfreezeFundDto unfreezeFundDto);
 
