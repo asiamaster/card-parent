a69d8088474a41c6be3b4a39bc4d4039f7961697
xuliang <514132096@qq.com>
2020-07-24 14:46:41 +0800

    重新对照资金项目枚举值；

--- card/src/main/java/com/dili/card/type/FundItem.java
+++ card/src/main/java/com/dili/card/type/FundItem.java
@@ -10,12 +10,12 @@ public enum FundItem {
     EBANK_CHARGE(3, "网银存款"),
     CASH_WITHDRAW(4, "现金取款"),
     EBANK_WITHDRAW(5, "网银取款"),
-    IC_CARD_COST(6, "IC工本费"),
+    IC_CARD_COST(6, "IC卡工本费"),
     EBANK_SERVICE(7, "网银手续费"),
     POS_SERVICE(8, "POS手续费"),
     TRADE_FREEZE(9, "交易冻结"),
     TRADE_UNFREEZE(10, "交易解冻"),
-    TRADE_FREEZE_UNFREEZE(11, "交易冻结解冻"),
+    RETURN_CARD_CHANGE(11, "退卡零钱"),
     MANDATORY_FREEZE_FUND(12, "手动冻结资金"),
     MANDATORY_UNFREEZE_FUND(13, "手动解冻资金"),
     MANDATORY_FREEZE_ACCOUNT(14, "手动冻结账户"),
@@ -24,12 +24,13 @@ public enum FundItem {
     CAR_ENTRANCE_FEE(17, "车辆进场费"),
     CAR_OUT_FEE(18, "车辆出场费"),
     WEIGH_SERVICE_FEE(19, "称重服务费"),
-    TRANSFER_FEE(20, "转场费"),
-    LEAVE_FEE(21, "离场费"),
+    TRANSFER_FEE(20, "车辆转场费"),
+    LEAVE_FEE(21, "车辆离场费"),
     TRADE_PAYMENT(22, "货款"),
     TRADE_SERVICE_FEE(23, "交易手续费"),
     PASSPORT_MANAGE_FEE(24, "通行证管理费"),
-    RETURN_CARD_SERVICE_FEE(25, "退卡手续费");
+    TEST_FEE(25, "检测费"),
+    QUERY_FEE(26, "查询费");
     private int code;
     private String name;
 
