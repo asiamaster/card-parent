799bf64adc1a372fb54a8f7995bdccb4d876891d
zhangxing <zhangxing@diligrp.com>
2020-07-29 10:09:11 +0800

    账户启用禁用状态

---
+++ card/src/main/java/com/dili/card/type/DisableState.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2019 www.diligrp.com All rights reserved. <br />
+ * 本软件源代码版权归地利集团,未经许可不得任意复制与传播.<br />
+ *
+ */
+package com.dili.card.type;
+
+/**
+ * 禁用启用状态
+ */
+public enum DisableState {
+	/** 启用 */
+	ENABLED(1, "启用"),
+	/** 禁用 */
+	DISABLED(2, "禁用");
+
+	private Integer code;
+	private String name;
+
+	private DisableState(Integer code, String name) {
+		this.code = code;
+		this.name = name;
+	}
+
+	public Integer getCode() {
+		return code;
+	}
+
+	public void setCode(Integer code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
