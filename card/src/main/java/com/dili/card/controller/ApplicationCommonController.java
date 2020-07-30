4255e4944cbb8935f4cf648a740b2dc79cdd4d34
miaoguoxin <398544597@qq.com>
2020-07-30 16:15:38 +0800

    增加字典接口

--- card/src/main/java/com/dili/card/controller/ApplicationCommonController.java
+++ card/src/main/java/com/dili/card/controller/ApplicationCommonController.java
@@ -1,16 +1,18 @@
 package com.dili.card.controller;
 
 import cn.hutool.core.util.IdUtil;
-import com.dili.card.common.annotation.ForbidDuplicateCommit;
 import com.dili.card.common.constant.CacheKey;
 import com.dili.ss.domain.BaseOutput;
 import com.dili.ss.redis.service.RedisUtil;
+import com.dili.uap.sdk.domain.DataDictionaryValue;
+import com.dili.uap.sdk.rpc.DataDictionaryRpc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -23,6 +25,8 @@ import java.util.concurrent.TimeUnit;
 public class ApplicationCommonController {
     @Autowired
     private RedisUtil redisUtil;
+    @Autowired
+    private DataDictionaryRpc dataDictionaryRpc;
 
     /**
      * 获取防重复提交的token
@@ -39,4 +43,14 @@ public class ApplicationCommonController {
         return BaseOutput.successData(id);
     }
 
+    /**
+     * 获取字典列表
+     * @author miaoguoxin
+     * @date 2020/7/30
+     */
+    @GetMapping("/dictList.action")
+    @ResponseBody
+    public BaseOutput<List<DataDictionaryValue>> getDataDictList(String code) {
+        return dataDictionaryRpc.listDataDictionaryValueByDdCode(code);
+    }
 }
