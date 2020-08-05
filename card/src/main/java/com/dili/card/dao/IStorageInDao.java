a14078e6380441604a58a559f8daf101b7a2ff7d
wangbo <wangbo@diligrp.com>
2020-08-05 18:19:21 +0800

    卡片批量入库修改

--- card/src/main/java/com/dili/card/dao/IStorageInDao.java
+++ card/src/main/java/com/dili/card/dao/IStorageInDao.java
@@ -2,6 +2,8 @@ package com.dili.card.dao;
 
 import org.apache.ibatis.annotations.Mapper;
 import java.util.List;
+
+import com.dili.card.dto.CardStorageOutQueryDto;
 import com.dili.card.entity.StorageInDo;
 
 /**
@@ -16,7 +18,7 @@ public interface IStorageInDao {
      * @param search
      * @return
      */
-	List<StorageInDo> selectList(StorageInDo storageIn);
+	List<StorageInDo> selectList(CardStorageOutQueryDto queryParam);
 
     /**
      * 新增
