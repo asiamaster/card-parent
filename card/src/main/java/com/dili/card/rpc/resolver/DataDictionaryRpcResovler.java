1c996f212928d0d3e71595fccb3646a0359d356c
zhangxing <zhangxing@diligrp.com>
2020-08-12 17:48:26 +0800

    数字字典

---
+++ card/src/main/java/com/dili/card/rpc/resolver/DataDictionaryRpcResovler.java
@@ -0,0 +1,36 @@
+package com.dili.card.rpc.resolver;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.dili.card.exception.CardAppBizException;
+import com.dili.ss.constant.ResultCode;
+import com.dili.ss.domain.BaseOutput;
+import com.dili.uap.sdk.domain.DataDictionaryValue;
+import com.dili.uap.sdk.rpc.DataDictionaryRpc;
+
+/**
+ * 数据字典
+ * @author zhangxing
+ */
+@Component
+public class DataDictionaryRpcResovler {
+	
+	@Autowired
+	private DataDictionaryRpc dataDictionaryRpc;
+
+    /**
+     * 根据业务类型获取数据字典
+     * @param type
+     * @return
+     */
+    public String findByDataDictionaryValue(String ddCode) {
+    	BaseOutput<List<DataDictionaryValue>> baseOutput = dataDictionaryRpc.listDataDictionaryValueByDdCode(ddCode);
+    	if (!baseOutput.isSuccess()) {
+			throw new CardAppBizException(ResultCode.DATA_ERROR, "远程调用数据字典服务失败");
+		}
+		return baseOutput.getData().get(0).getCode();
+    }
+}
