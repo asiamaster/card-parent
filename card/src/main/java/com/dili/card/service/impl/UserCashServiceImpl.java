123ee80e3c8591529a2f1686efbf38fb6d453fcd
zhangxing <zhangxing@diligrp.com>
2020-09-14 15:24:30 +0800

    修改

--- card/src/main/java/com/dili/card/service/impl/UserCashServiceImpl.java
+++ card/src/main/java/com/dili/card/service/impl/UserCashServiceImpl.java
@@ -92,8 +92,10 @@ public class UserCashServiceImpl implements IUserCashService {
 		// 金额校验
 		this.validateAmount(userCashDto.getAmount(), userCashDto.getSettledApply());
 		// 获取账务周期
-		AccountCycleDo accountCycle = accountCycleService.findActiveCycleByUserId(userCashDto.getUserId(),
-				userCashDto.getUserName(), userCashDto.getUserCode());
+		AccountCycleDo accountCycle = accountCycleService.findLatestCycleByUserId(userCashDto.getUserId());
+		if (!accountCycle.getState().equals(CycleState.ACTIVE.getCode())) {
+			throw new CardAppBizException(ResultCode.DATA_ERROR, "账期状态已[" + CycleState.getNameByCode(accountCycle.getState()) + "不能删除");
+		}
 		AccountCycleDto accountCycleDto = accountCycleService.detail(accountCycle.getId());
 		// 校验现金余额 领款修改不能导致现金余额小于0 并且校验是本人
 		if (userCashDto.getAction().equals(CashAction.PAYEE.getCode())
