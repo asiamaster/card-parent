e8d78409877544a3411f98d75192c7ceba07d46f
zhangxing <zhangxing@diligrp.com>
2020-09-19 09:30:00 +0800

    dfs

--- card/src/main/java/com/dili/card/rpc/DFSRpc.java
+++ card/src/main/java/com/dili/card/rpc/DFSRpc.java
@@ -1,6 +1,7 @@
 package com.dili.card.rpc;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,10 +10,13 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.dili.ss.domain.BaseOutput;
 
+import feign.codec.Encoder;
+import feign.form.spring.SpringFormEncoder;
+
 /**
  * 文件上传
  */
-@FeignClient(name = "dili-dfs")
+@FeignClient(name = "dili-dfs", configuration = DFSRpc.FileConfiguration.class)
 public interface DFSRpc {
 
     /**
@@ -20,4 +24,12 @@ public interface DFSRpc {
      */
     @PostMapping(value = "/file/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     BaseOutput<String> upload(@RequestPart("file") MultipartFile multipartFile, @RequestParam("accesstoken") String accesstoken);
+    
+    class FileConfiguration{
+
+        @Bean
+        public Encoder encoder(){
+            return new SpringFormEncoder();
+        }
+    }
 }
