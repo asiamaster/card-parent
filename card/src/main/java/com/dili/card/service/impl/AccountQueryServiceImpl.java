70ecf7f0366bf1b579db2775b6f0a0ce460a68c8
miaoguoxin <398544597@qq.com>
2020-09-22 15:31:10 +0800

    卡提示

--- card/src/main/java/com/dili/card/service/impl/AccountQueryServiceImpl.java
+++ card/src/main/java/com/dili/card/service/impl/AccountQueryServiceImpl.java
@@ -159,7 +159,7 @@ public class AccountQueryServiceImpl implements IAccountQueryService {
             throw new CardAppBizException(ResultCode.DATA_ERROR, "该卡不是挂失状态，不能进行此操作");
         }
         if (DisableState.DISABLED.getCode().equals(single.getDisabledState())) {
-            throw new CardAppBizException(ResultCode.DATA_ERROR, "该账户为禁用状态，不能进行此操作");
+            throw new CardAppBizException(ResultCode.DATA_ERROR, String.format("该账户为%s状态，不能进行此操作", DisableState.DISABLED.getName()));
         }
         return single;
     }
