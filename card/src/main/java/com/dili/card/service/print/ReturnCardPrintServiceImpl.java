f62624636d72c68dbffbdb222c108461d5222cae
zhangxing <zhangxing@diligrp.com>
2020-09-25 10:29:21 +0800

    return

--- card/src/main/java/com/dili/card/service/print/ReturnCardPrintServiceImpl.java
+++ card/src/main/java/com/dili/card/service/print/ReturnCardPrintServiceImpl.java
@@ -1,9 +1,13 @@
 package com.dili.card.service.print;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
+import com.alibaba.fastjson.JSONObject;
+import com.dili.card.common.constant.Constant;
 import com.dili.card.dto.PrintDto;
 import com.dili.card.entity.BusinessRecordDo;
+import com.dili.card.type.CardType;
 import com.dili.card.type.OperateType;
 import com.dili.card.type.PrintTemplate;
 import com.dili.card.util.CurrencyUtils;
@@ -27,6 +31,13 @@ public class ReturnCardPrintServiceImpl extends PrintServiceImpl{
     	Long totalAmount = 0L;
         printDto.setTotalAmount(CurrencyUtils.cent2TenNoSymbol(totalAmount));
         printDto.setTotalAmountWords(Convert.digitToChinese(Double.valueOf(printDto.getTotalAmount())));
+        String attach = recordDo.getAttach();
+        if (StringUtils.isNotBlank(attach)) {
+			Integer cardType = JSONObject.parseObject(attach).getInteger(Constant.BUSINESS_RECORD_ATTACH_CARDTYPE);
+			if(CardType.isSlave(cardType)) {
+				printDto.setBalance("");
+			}
+		}
     }
 
     @Override
