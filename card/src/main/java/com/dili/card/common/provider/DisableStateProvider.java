19e4577722e795c0ebaeaeeab185b52b44a283ec
miaoguoxin <398544597@qq.com>
2020-09-27 15:40:15 +0800

    页面调整

--- card/src/main/java/com/dili/card/common/provider/DisableStateProvider.java
+++ card/src/main/java/com/dili/card/common/provider/DisableStateProvider.java
@@ -3,8 +3,11 @@ package com.dili.card.common.provider;
 import com.dili.card.type.DisableState;
 import com.dili.ss.metadata.FieldMeta;
 import com.dili.ss.metadata.ValuePair;
+import com.dili.ss.metadata.ValuePairImpl;
 import com.dili.ss.metadata.ValueProvider;
+import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -13,11 +16,17 @@ import java.util.Map;
  * @Date: 2020/7/29 09:54
  * @Description:
  */
+@Component
 public class DisableStateProvider implements ValueProvider {
+    private static final List<ValuePair<?>> BUFFER = new ArrayList<>();
 
+    static {
+        new ArrayList<>(DisableState.getAll())
+                .forEach(c -> BUFFER.add(new ValuePairImpl(c.getName(), c.getCode())));
+    }
     @Override
     public List<ValuePair<?>> getLookupList(Object val, Map metaMap, FieldMeta fieldMeta) {
-        return null;
+        return BUFFER;
     }
 
     @Override
