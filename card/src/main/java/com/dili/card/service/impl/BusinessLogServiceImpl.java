ce2f8fb265f0b084366d06e5f513983101e7931f
miaoguoxin <398544597@qq.com>
2020-10-30 13:02:44 +0800

    操作日志记录ip

--- card/src/main/java/com/dili/card/service/impl/BusinessLogServiceImpl.java
+++ card/src/main/java/com/dili/card/service/impl/BusinessLogServiceImpl.java
@@ -1,14 +1,6 @@
 package com.dili.card.service.impl;
 
-import java.time.LocalDateTime;
-
-import javax.annotation.Resource;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Service;
-
+import cn.hutool.core.thread.ThreadUtil;
 import com.dili.card.common.constant.ServiceName;
 import com.dili.card.rpc.resolver.GenericRpcResolver;
 import com.dili.card.service.IBusinessLogService;
@@ -16,55 +8,71 @@ import com.dili.card.type.LogOperationType;
 import com.dili.card.type.OperateType;
 import com.dili.logger.sdk.domain.BusinessLog;
 import com.dili.logger.sdk.rpc.BusinessLogRpc;
+import com.dili.ss.mvc.util.RequestUtils;
 import com.dili.uap.sdk.domain.UserTicket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 @Service("businessLogService")
 @SuppressWarnings("unchecked")
 public class BusinessLogServiceImpl implements IBusinessLogService {
 
-	/** 日志来源,日志服务校验格式*.diligrp.com */
-	private static final String LOG_REFERER = "http://card.diligrp.com";
+    /** 日志来源,日志服务校验格式*.diligrp.com */
+    private static final String LOG_REFERER = "http://card.diligrp.com";
 
-	/** 日志所属系统，UAP中系统管理 - 系统编码 */
-	private static final String LOG_SYSTEM_CODE = "CARD";
+    /** 日志所属系统，UAP中系统管理 - 系统编码 */
+    private static final String LOG_SYSTEM_CODE = "CARD";
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(BusinessLogServiceImpl.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(BusinessLogServiceImpl.class);
 
-	@Resource
-	BusinessLogRpc businessLogRpc;
+    @Resource
+    private BusinessLogRpc businessLogRpc;
+    @Autowired
+    private HttpServletRequest request;
 
-	
-	@Override
-	@Async
-	public void saveLog(OperateType operateType, UserTicket userTicket, String... content) {
-		try {
-			saveLog(operateType, LogOperationType.SUBMIT, userTicket, content);
-		} catch (Exception e) {
-			LOGGER.error("{}保存操作日志失败,内容【{}】!", operateType.getName(), content);
-			LOGGER.error("save log error!", e);
-		}
-	}
 
-	@Override
-	@Async
-	public void saveLog(OperateType operateType, LogOperationType logOperationType, UserTicket userTicket,
-			String... content) {
-		try {
-			BusinessLog log = new BusinessLog();
-			log.setOperatorId(userTicket.getId());
-			log.setOperatorName(userTicket.getRealName());
-			log.setBusinessType(operateType.getCode() + "");
-			log.setMarketId(userTicket.getFirmId());
-			log.setCreateTime(LocalDateTime.now());
-			log.setOperationType(logOperationType.getCode());
-			if (content != null && content.length != 0) {
-				log.setContent(String.join(",", content));
-			}
-			log.setSystemCode(LOG_SYSTEM_CODE);
-			GenericRpcResolver.resolver(businessLogRpc.save(log, LOG_REFERER), ServiceName.LOGGER);
-		} catch (Exception e) {
-			LOGGER.error("{}保存操作日志失败,内容【{}】!", operateType.getName(), content);
-			LOGGER.error("save log error!", e);
-		}
-	}
+    @Override
+    public void saveLog(OperateType operateType, UserTicket userTicket, String... content) {
+        try {
+            saveLog(operateType, LogOperationType.SUBMIT, userTicket, content);
+        } catch (Exception e) {
+            LOGGER.error("{}保存操作日志失败,内容【{}】!", operateType.getName(), content);
+            LOGGER.error("save log error!", e);
+        }
+    }
+
+    @Override
+    public void saveLog(OperateType operateType, LogOperationType logOperationType, UserTicket userTicket,
+                        String... content) {
+        String remoteIp = RequestUtils.getIpAddress(request);
+        String serverIp = request.getLocalAddr();
+        ThreadUtil.execute(() -> {
+            try {
+                BusinessLog log = new BusinessLog();
+                log.setRemoteIp(remoteIp);
+                log.setServerIp(serverIp);
+                log.setOperatorId(userTicket.getId());
+                log.setOperatorName(userTicket.getRealName());
+                log.setBusinessType(operateType.getCode() + "");
+                log.setMarketId(userTicket.getFirmId());
+                log.setCreateTime(LocalDateTime.now());
+                log.setOperationType(logOperationType.getCode());
+                if (content != null && content.length != 0) {
+                    log.setContent(String.join(",", content));
+                }
+                log.setSystemCode(LOG_SYSTEM_CODE);
+                GenericRpcResolver.resolver(businessLogRpc.save(log, LOG_REFERER), ServiceName.LOGGER);
+            } catch (Exception e) {
+                LOGGER.error("{}保存操作日志失败,内容【{}】!", operateType.getName(), content);
+                LOGGER.error("save log error!", e);
+            }
+        });
+    }
+
 }
