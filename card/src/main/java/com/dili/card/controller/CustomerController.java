bde7bfbbedf17d171787a255de3c13d832337253
wangbo <wangbo@diligrp.com>
2020-12-15 11:30:46 +0800

    增加持卡人电话及标志，为C端增加查询客户子类型接口

--- card/src/main/java/com/dili/card/controller/CustomerController.java
+++ card/src/main/java/com/dili/card/controller/CustomerController.java
@@ -2,11 +2,14 @@ package com.dili.card.controller;
 
 import com.dili.card.common.handler.IControllerHandler;
 import com.dili.card.rpc.resolver.CustomerRpcResolver;
+import com.dili.card.service.ICustomerService;
 import com.dili.customer.sdk.domain.Customer;
 import com.dili.customer.sdk.domain.dto.CustomerExtendDto;
 import com.dili.customer.sdk.domain.dto.CustomerQueryInput;
 import com.dili.ss.domain.BaseOutput;
 import com.dili.uap.sdk.domain.UserTicket;
+import com.google.common.collect.Lists;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 客户相关controller
@@ -25,6 +29,9 @@ public class CustomerController implements IControllerHandler {
     private static final Logger LOGGER = LoggerFactory.getLogger(CustomerController.class);
     @Resource
     private CustomerRpcResolver customerRpcResolver;
+    
+    @Resource
+    private ICustomerService customerService;
 
     /**
      * 查询客户列表
@@ -60,4 +67,16 @@ public class CustomerController implements IControllerHandler {
         return BaseOutput.successData(itemList);
     }
 
+    /**
+     * 根据客户ID获取客户子类型
+     */
+    @RequestMapping(value = "/getSubTypeName.action")
+    @ResponseBody
+    public BaseOutput<Map<Long, String>> getSubTypeName(Long customerId) {
+        LOGGER.info("获取客户子类型*****{}", customerId);
+        UserTicket userTicket = getUserTicket();
+        Map<Long, String> subTypeNames = customerService.getSubTypeNames(Lists.newArrayList(customerId), userTicket.getFirmId());
+        return BaseOutput.successData(subTypeNames);
+    }
+    
 }
--- card/src/main/java/com/dili/card/dto/OpenCardDto.java
+++ card/src/main/java/com/dili/card/dto/OpenCardDto.java
@@ -61,6 +61,12 @@ public class OpenCardDto extends BaseSerialLogDto {
 	private Long costFee;
 	/** 持卡人 */
 	private String holdName;
+	/** 持卡人证件号 */
+	private String holdCertificateNumber;
+	/** 持卡人联系电话 */
+	private String holdContactsPhone;
+	/** 客户信息修改时是否同步修改持卡人信息1-是，2-否 */
+	private Integer customerSyncModifyHoldinfo;
 
 	public Long getCustomerId() {
 		return customerId;
@@ -244,6 +250,30 @@ public class OpenCardDto extends BaseSerialLogDto {
 
 	public void setCustomerCharacterTypeList(List<CharacterType> customerCharacterTypeList) {
 		this.customerCharacterTypeList = customerCharacterTypeList;
+	}
+
+	public String getHoldCertificateNumber() {
+		return holdCertificateNumber;
+	}
+
+	public void setHoldCertificateNumber(String holdCertificateNumber) {
+		this.holdCertificateNumber = holdCertificateNumber;
+	}
+
+	public String getHoldContactsPhone() {
+		return holdContactsPhone;
+	}
+
+	public void setHoldContactsPhone(String holdContactsPhone) {
+		this.holdContactsPhone = holdContactsPhone;
+	}
+
+	public Integer getCustomerSyncModifyHoldinfo() {
+		return customerSyncModifyHoldinfo;
+	}
+
+	public void setCustomerSyncModifyHoldinfo(Integer customerSyncModifyHoldinfo) {
+		this.customerSyncModifyHoldinfo = customerSyncModifyHoldinfo;
 	}
 
 }
