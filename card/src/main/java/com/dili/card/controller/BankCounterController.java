a0d40152d348c86852ad0d4e0def2fa4a103dc93
miaoguoxin <398544597@qq.com>
2021-01-07 15:27:29 +0800

    银行存取款时间跨度限制

--- card/src/main/java/com/dili/card/controller/BankCounterController.java
+++ card/src/main/java/com/dili/card/controller/BankCounterController.java
@@ -1,14 +1,17 @@
 package com.dili.card.controller;
 
+import cn.hutool.core.date.DateUnit;
 import cn.hutool.core.date.DateUtil;
 import com.dili.card.common.handler.IControllerHandler;
 import com.dili.card.dto.BankCounterQuery;
 import com.dili.card.dto.BankCounterRequestDto;
 import com.dili.card.dto.BankCounterResponseDto;
+import com.dili.card.exception.CardAppBizException;
 import com.dili.card.service.IBankCounterService;
 import com.dili.card.validator.ConstantValidator;
 import com.dili.ss.domain.BaseOutput;
 import com.dili.ss.domain.PageOutput;
+import com.dili.ss.util.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -19,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -54,10 +58,12 @@ public class BankCounterController implements IControllerHandler {
     public Map<String, Object> page(@Validated(ConstantValidator.Page.class)
                                             BankCounterQuery param) {
         // this.buildOperatorInfo(param);
+        this.validateTime(param);
         PageOutput<List<BankCounterResponseDto>> page = bankCounterService.getPage(param);
         return successPage(page);
 
     }
+
 
     /**
      * 跳转新增存款页面
@@ -107,5 +113,22 @@ public class BankCounterController implements IControllerHandler {
         buildOperatorInfo(requestDto);
         bankCounterService.add(requestDto);
         return BaseOutput.success();
+    }
+
+    /**
+    * 校验时间
+    * @author miaoguoxin
+    * @date 2021/1/7
+    */
+    private void validateTime(BankCounterQuery param) {
+        if (param.getStartDate() == null || param.getEndDate() == null) {
+            return;
+        }
+        Date startDate = DateUtils.localDateTimeToUdate(param.getStartDate());
+        Date endDate = DateUtils.localDateTimeToUdate(param.getEndDate());
+        long diffDay = DateUtil.between(startDate, endDate, DateUnit.DAY, true);
+        if (diffDay > 365) {
+            throw new CardAppBizException("查询时间跨度不能超过365天");
+        }
     }
 }
