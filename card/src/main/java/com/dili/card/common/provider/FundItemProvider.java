e16582446eff17075ed14c4ec285e7c6a46a0dd7
wangbo <wangbo@diligrp.com>
2021-01-26 18:18:56 +0800

    资金交易记录中的资金项目动态从配置项目中获取

--- card/src/main/java/com/dili/card/common/provider/FundItemProvider.java
+++ card/src/main/java/com/dili/card/common/provider/FundItemProvider.java
@@ -1,12 +1,18 @@
 package com.dili.card.common.provider;
 
+import com.alibaba.fastjson.JSONObject;
+import com.dili.assets.sdk.dto.ConfigDto;
+import com.dili.assets.sdk.dto.ConfigQuery;
 import com.dili.assets.sdk.rpc.ConfigRpc;
 import com.dili.card.type.FundItem;
+import com.dili.commons.bstable.TableResult;
 import com.dili.ss.metadata.FieldMeta;
 import com.dili.ss.metadata.ValuePair;
 import com.dili.ss.metadata.ValuePairImpl;
 import com.dili.ss.metadata.ValueProvider;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -21,15 +27,25 @@ import java.util.stream.Stream;
  */
 @Component
 public class FundItemProvider implements ValueProvider {
+	
+	private static final Logger log = LoggerFactory.getLogger(FundItemProvider.class);
+
     private static final List<ValuePair<?>> BUFFER = new ArrayList<>();
     @Autowired
     private ConfigRpc configRpc;
 
-    static {
-        BUFFER.addAll(Stream.of(FundItem.values()).map(temp -> new ValuePairImpl<>(temp.getName(), temp.getCode())).collect(Collectors.toList()));
-    }
+//    static {
+//        BUFFER.addAll(Stream.of(FundItem.values()).map(temp -> new ValuePairImpl<>(temp.getName(), temp.getCode())).collect(Collectors.toList()));
+//    }
     @Override
     public List<ValuePair<?>> getLookupList(Object val, Map metaMap, FieldMeta fieldMeta) {
+    	ConfigQuery query =new ConfigQuery();
+    	query.setType("fundItem");
+    	TableResult<ConfigDto> fundItemList = configRpc.query(query);
+    	log.info("资金项目返回>{}",JSONObject.toJSONString(fundItemList));
+    	List<ConfigDto> rows = fundItemList.getRows();
+    	BUFFER.addAll(rows.stream().map(temp -> new ValuePairImpl<>(
+					temp.getName(), temp.getId())).collect(Collectors.toList()));
         return BUFFER;
     }
 
