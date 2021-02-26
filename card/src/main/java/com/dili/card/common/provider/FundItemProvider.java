81f69ad58a5e698a6331f204f2ea8622b3218a3a
wangbo <wangbo@diligrp.com>
2021-02-26 14:15:09 +0800

    更新获取资金项目接口

--- card/src/main/java/com/dili/card/common/provider/FundItemProvider.java
+++ card/src/main/java/com/dili/card/common/provider/FundItemProvider.java
@@ -3,14 +3,24 @@ package com.dili.card.common.provider;
 import cn.hutool.cache.CacheUtil;
 import cn.hutool.cache.impl.TimedCache;
 import cn.hutool.core.collection.CollectionUtil;
+
+import com.alibaba.fastjson.JSONObject;
 import com.dili.assets.sdk.dto.ConfigDto;
 import com.dili.assets.sdk.dto.ConfigQuery;
+import com.dili.assets.sdk.dto.FunditemDto;
+import com.dili.assets.sdk.dto.FunditemQuery;
 import com.dili.assets.sdk.rpc.ConfigRpc;
+import com.dili.assets.sdk.rpc.FunditemRpc;
+import com.dili.card.common.constant.ServiceName;
+import com.dili.card.rpc.resolver.GenericRpcResolver;
 import com.dili.commons.bstable.TableResult;
+import com.dili.ss.domain.BaseOutput;
 import com.dili.ss.metadata.FieldMeta;
 import com.dili.ss.metadata.ValuePair;
 import com.dili.ss.metadata.ValuePairImpl;
 import com.dili.ss.metadata.ValueProvider;
+import com.dili.uap.sdk.session.SessionContext;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,24 +42,19 @@ public class FundItemProvider implements ValueProvider {
 
     private static final List<ValuePair<?>> BUFFER = new ArrayList<>();
     @Autowired
-    private ConfigRpc configRpc;
+    private FunditemRpc funditemRpc;
 
-    private TimedCache<String, List<ConfigDto>> cache = CacheUtil.newTimedCache(1000 * 60 * 10);
 
     @Override
     public List<ValuePair<?>> getLookupList(Object val, Map metaMap, FieldMeta fieldMeta) {
-        final String key = "items";
-        List<ConfigDto> configs = cache.get(key);
-        if (CollectionUtil.isEmpty(configs)) {
-            BUFFER.clear();
-            ConfigQuery query = new ConfigQuery();
-            query.setType("fundItem");
-            TableResult<ConfigDto> fundItemList = configRpc.query(query);
-            List<ConfigDto> rows = fundItemList.getRows();
-            cache.put(key, rows);
-            BUFFER.addAll(rows.stream().map(temp -> new ValuePairImpl<>(
-                    temp.getName(), temp.getId())).collect(Collectors.toList()));
-        }
+        BUFFER.clear();
+        Long firmId = SessionContext.getSessionContext().getUserTicket().getFirmId();
+        FunditemQuery query =new FunditemQuery();
+        query.setMarketId(firmId);
+        List<FunditemDto> fundItemList = GenericRpcResolver.resolver(funditemRpc.queryAll(query), ServiceName.ASSETS);
+        log.info("市场{}资金项目列表{}",firmId,JSONObject.toJSONString(fundItemList));
+        BUFFER.addAll(fundItemList.stream().map(temp -> new ValuePairImpl<>(
+              temp.getName(), temp.getId())).collect(Collectors.toList()));
         if (val != null){
             String conditionVal = (String) val;
             return BUFFER.stream()
